78c81d93f6510b7335e40b4569630280dee6dd88
graphica99 <mendsalbert@gmail.com>
2022-04-15 11:41:56 +0000

    backend end

--- routes/order.js
+++ routes/order.js
@@ -6,6 +6,8 @@ const {
   getUserOrders,
   updateStatus,
   pendingOrderController,
+  updateFeedbackController,
+  updateRefundController,
 } = require("../controllers/order");
 const { authenticated } = require("../middlewares/authenticate");
 //!!delete order will be added later
@@ -24,4 +26,8 @@ router.get("/pending-orders", pendingOrderController);
 router.get("/user-orders", authenticated, getUserOrders);
 
 router.post("/update-status/:status/:id", updateStatus);
+
+router.post("/update-feedback/:id", updateFeedbackController);
+router.post("/update-refund/:id", updateRefundController);
+
 module.exports = router;
